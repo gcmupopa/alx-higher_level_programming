0a15cbd1d09db9ed8c9bdc0ca1ac45d4e3b02b5b
root <gcmupopa@gmail.com>
2023-08-12 07:27:48 -0700

    included shebang in task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,14 @@
-import sys
+#!/usr/bin/python3
 
-args = sys.argv[1:]
-number_args = len(args)
+if __name__ == '__main__':
+    import sys
 
-if number_args == 0:
-    print("0 arguments.")
-else:
-    print(f"{number_args} argument{'s' if number_args > 1 else ''}:")
-    for index in range(number_args):
-        print(f"{index+1}: {args[i]}")
+    args = sys.argv[1:]
+    number_args = len(args)
+
+    if number_args == 0:
+        print("0 arguments.")
+    else:
+        print(f"{number_args} argument{'s' if number_args > 1 else ''}:")
+        for index in range(number_args):
+            print(f"{index+1}: {args[index]}")
