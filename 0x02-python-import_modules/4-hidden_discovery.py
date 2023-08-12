8ed8fe9c94f738c1a338937d82f3d0d14da0903b
root <gcmupopa@gmail.com>
2023-08-12 13:49:36 -0700

    Task 4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import sys
+import hidden_4
+
+if __name__ == '__main__':
+
+    if sys.version_info[:2] != (3,8):
+        sys.exit()
+
+    nam = dir(hidden_4)
+    finam = [name for name in nam if not name.startswith("__")]
+    sotname = sorted(finam)
+
+    for name in sotname:
+        print(name)
