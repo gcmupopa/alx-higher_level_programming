1f97cbcdc412cea380c8c283a514ec9fcf0d553b
root <gcmupopa@gmail.com>
2023-08-12 02:44:43 -0700

    fixed pep8 issues

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-a = 1
-b = 2
+    a = 1
+    b = 2
 
-modu = __import__('add_0')
-add = modu.add
+    modu = __import__('add_0')
+    add = modu.add
 
-summ = add(a, b)
-print("{} + {} = {}".format(a, b, summ))
+    summ = add(a, b)
+    print("{} + {} = {}\n".format(a, b, summ))
