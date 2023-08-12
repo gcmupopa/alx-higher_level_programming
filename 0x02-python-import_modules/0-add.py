2b73ae1b3f0d2d0d78b05999476f24c2b157a2ac
root <gcmupopa@gmail.com>
2023-08-12 03:55:22 -0700

    included add function in print

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
-
-    modu = __import__('add_0')
-    add = modu.add
 
     print("{} + {} = {}\n".format(a, b, add(a, b)))
