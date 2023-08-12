c008f5145605a4d2f61637265e997c244f8bcaec
root <gcmupopa@gmail.com>
2023-08-12 07:14:14 -0700

    fixed pep8 issues in task 2

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,16 @@
-from calculator_1.py import add, sub, mul, div
+#!/usr/bin/python3
 
-a = 10
-b = 5
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
 
-ad = add(a, b)
-su = sub(a, b)
-mu = mul(a, b)
-di = div(a, b)
-print("{} + {} = {}".format(a, b, ad))
-print("{} - {} = {}".format(a, b, su))
-print("{} * {} = {}".format(a, b, mu))
-print("{} / {} = {}".format(a, b, di))
+    ad = add(a, b)
+    su = sub(a, b)
+    mu = mul(a, b)
+    di = div(a, b)
+
+    print("{} + {} = {}".format(a, b, ad))
+    print("{} - {} = {}".format(a, b, su))
+    print("{} * {} = {}".format(a, b, mu))
+    print("{} / {} = {}".format(a, b, di))
