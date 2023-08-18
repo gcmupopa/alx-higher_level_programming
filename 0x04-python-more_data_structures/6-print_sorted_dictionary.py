c800e87186f386b05ff63f0db63a58a767d08944
root <gcmupopa@gmail.com>
2023-08-18 11:35:11 -0700

    removed space between key and :

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,4 +4,4 @@ def print_sorted_dictionary(a_dictionary):
     keysort = sorted(a_dictionary.keys())
 
     for key in keysort:
-        print(key, ":", a_dictionary[key])
+        print('{}: {}'.format(key, a_dictionary[key]))
