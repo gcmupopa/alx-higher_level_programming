5f1a457c4213701c51a4db3e7d1ef59e7af5ea9d
root <gcmupopa@gmail.com>
2023-08-18 11:47:35 -0700

    removed else part in task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,6 +3,4 @@
 def update_dictionary(a_dictionary, key, value):
     if key in a_dictionary:
         a_dictionary[key] = value
-    else:
-        a_dictionary[key] = value
     return a_dictionary
