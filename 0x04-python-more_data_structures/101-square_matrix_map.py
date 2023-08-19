5b27c0e2f7ebf438d84481aa00e63a5964d5e5c0
root <gcmupopa@gmail.com>
2023-08-18 22:34:01 -0700

    replaced the undefined matrix by my_list in task 14

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-
 def square_matrix_map(my_list=[]):
-    return list(map(lambda x, m=matrix: list(map(lambda y, m=m: y**2, x)), matrix))
+    return list(map(lambda x: list(map(lambda y: y**2, x)), my_list))
