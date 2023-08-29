8415ec8a0e66981a626adc5b526fc5f6ccd6547f
root <gcmupopa@gmail.com>
2023-08-29 05:24:09 -0700

    task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+This is module that defines Square class.
+"""
+
+class Square:
+    """
+    A class that represents a square.
+
+    Attributes:
+    - sidelen (float): Length of square's sides
+    """
+
+    pass
