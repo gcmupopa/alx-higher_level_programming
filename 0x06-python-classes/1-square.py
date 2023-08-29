c45d1005c620d4e51b2892ba0918753a4e0c8e0b
root <gcmupopa@gmail.com>
2023-08-29 05:42:37 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+This is module that defines Square class.
+"""
+
+
+class Square:
+    """
+    A class that represents a square.
+
+    Attributes:
+    - size (float): Length of square's sides
+    """
+
+    def __init__(self, size):
+        self.__size = size
