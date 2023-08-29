d16dd67af054a212513b46f59e1f121fd8f2df01
root <gcmupopa@gmail.com>
2023-08-29 06:11:42 -0700

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+This is module that defines Square class.
+"""
+
+
+class Square:
+    """
+    A class that represents a square.
+
+    Attributes:
+    - size (float): Length of square's sides
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an int")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
