c8bbd1d60b74606ccc35e5320fd93bfe1c414551
root <gcmupopa@gmail.com>
2023-09-12 19:04:52 -0700

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+
+"""
+This module defines read_file that reads text file and prints to stdout
+"""
+
+
 def read_file(filename=""):
     """
     Function that reads a text file and prints its contents to stdout
