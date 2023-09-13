2e9d1f72d3c522b8030f5f8c1ef7c805ecbf7bdf
root <gcmupopa@gmail.com>
2023-09-12 18:59:30 -0700

    fixed pep8 in task 0-2

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,14 +2,14 @@
 def read_file(filename=""):
     """
     Function that reads a text file and prints its contents to stdout
-        
+
     Args:
         filename: name of file to be read
-            
+
     Returns:
         None
     """
-        
+
     with open(filename, 'r', encoding="UTF-8") as file:
         for line in file:
             print(line, end="")
